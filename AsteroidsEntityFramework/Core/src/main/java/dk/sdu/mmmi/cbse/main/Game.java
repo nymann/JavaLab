99a8af484461ccc5041899b06229e3dde87057b5
Kristian Nymann Jakobsen <kristian@nymann.dev>
2022-05-13 00:59:35 +0200

    :construction: Implement SPI (works in IDE not via make)

--- AsteroidsEntityFramework/Core/src/main/java/dk/sdu/mmmi/cbse/main/Game.java
+++ AsteroidsEntityFramework/Core/src/main/java/dk/sdu/mmmi/cbse/main/Game.java
@@ -5,12 +5,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import dk.sdu.mmmi.cbse.asteroidsplittingsystem.AsteroidSplitter;
-import dk.sdu.mmmi.cbse.asteroidsystem.AsteroidControlSystem;
-import dk.sdu.mmmi.cbse.asteroidsystem.AsteroidPlugin;
-import dk.sdu.mmmi.cbse.bulletsystem.BulletControlSystem;
-import dk.sdu.mmmi.cbse.bulletsystem.BulletPlugin;
-import dk.sdu.mmmi.cbse.collisiondetectionsystem.CollisionDetection;
 import dk.sdu.mmmi.cbse.common.IShapeRender;
 import dk.sdu.mmmi.cbse.common.MyShapeRender;
 import dk.sdu.mmmi.cbse.common.data.Entity;
@@ -19,23 +13,17 @@ import dk.sdu.mmmi.cbse.common.data.World;
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
 import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
 import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
-import dk.sdu.mmmi.cbse.enemysystem.EnemyControlSystem;
-import dk.sdu.mmmi.cbse.enemysystem.EnemyPlugin;
-import dk.sdu.mmmi.cbse.lifeprocessersystem.LifeProcesser;
+import dk.sdu.mmmi.cbse.common.spi.SPILocator;
 import dk.sdu.mmmi.cbse.managers.GameInputProcessor;
-import dk.sdu.mmmi.cbse.playersystem.PlayerControlSystem;
-import dk.sdu.mmmi.cbse.playersystem.PlayerPlugin;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
 
 public class Game implements ApplicationListener {
 
     private final GameData gameData = new GameData();
-    private final List<IEntityProcessingService> entityProcessors = new ArrayList<>();
-    private final List<IPostEntityProcessingService> entityPostProcessors = new ArrayList<>();
-    private final List<IGamePluginService> entityPlugins = new ArrayList<>();
     private final World world = new World();
+    private Collection<? extends IPostEntityProcessingService> postEntityProcessingServices;
+    private Collection<? extends IEntityProcessingService> entityProcessingServices;
     private IShapeRender sr;
 
     @Override
@@ -52,23 +40,11 @@ public class Game implements ApplicationListener {
 
         Gdx.input.setInputProcessor(new GameInputProcessor(gameData));
 
-        entityPostProcessors.add(new CollisionDetection());
-        entityPostProcessors.add(new LifeProcesser());
-        entityPostProcessors.add(new AsteroidSplitter());
-
-        entityPlugins.add(new PlayerPlugin());
-        entityPlugins.add(new EnemyPlugin());
-        entityPlugins.add(new BulletPlugin());
-        entityPlugins.add(new AsteroidPlugin());
-
-        entityProcessors.add(new PlayerControlSystem());
-        entityProcessors.add(new AsteroidControlSystem());
-        entityProcessors.add(new BulletControlSystem());
-        entityProcessors.add(new EnemyControlSystem());
-
-        for (IGamePluginService iGamePlugin : entityPlugins) {
+        for (IGamePluginService iGamePlugin : getPluginServices()) {
             iGamePlugin.start(gameData, world);
         }
+        entityProcessingServices = getEntityProcessingServices();
+        postEntityProcessingServices = getPostEntityProcessingServices();
     }
 
     @Override
@@ -86,12 +62,11 @@ public class Game implements ApplicationListener {
     }
 
     private void update() {
-        for (IEntityProcessingService entityProcessorService : entityProcessors) {
+        for (IEntityProcessingService entityProcessorService : entityProcessingServices) {
             entityProcessorService.process(gameData, world);
         }
-
-        for (IPostEntityProcessingService postEntityProcessingService : entityPostProcessors) {
-            postEntityProcessingService.process(gameData, world);
+        for (IPostEntityProcessingService postEntityProcessorService : postEntityProcessingServices) {
+            postEntityProcessorService.process(gameData, world);
         }
     }
 
@@ -115,5 +90,17 @@ public class Game implements ApplicationListener {
 
     @Override
     public void dispose() {
+    }
+
+    private Collection<? extends IGamePluginService> getPluginServices() {
+        return SPILocator.locateAll(IGamePluginService.class);
+    }
+
+    private Collection<? extends IEntityProcessingService> getEntityProcessingServices() {
+        return SPILocator.locateAll(IEntityProcessingService.class);
+    }
+
+    private Collection<? extends IPostEntityProcessingService> getPostEntityProcessingServices() {
+        return SPILocator.locateAll(IPostEntityProcessingService.class);
     }
 }
